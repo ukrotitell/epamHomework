c6843bdeb442e68eb68940e7370cff9d19468751
ukrotitell <34740218+ukrotitell@users.noreply.github.com>
2022-01-10 20:02:25 +0400

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.format("Hello " + System.getenv("STR") + "!" + "!");
+        System.out.format("Hello " + System.getenv("STR") + "!" + "!);
     }
 }
